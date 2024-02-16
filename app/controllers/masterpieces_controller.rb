64bd80d43d4560382fe9c88c4f501b2499890970
francois lorfevre <francoislorfevre@hotmail.com>
2024-02-16 16:57:23 +0100

    route + seedTEST + controller masterpiece + index_view

---
+++ app/controllers/masterpieces_controller.rb
@@ -0,0 +1,27 @@
+class MasterpiecesController < ApplicationController
+
+def index
+  @masterpieces = Masterpiece.all
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+def destroy
+end
+
+private
+
+def set_masterpiece
+end
+
+def masterpiece_params
+end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning databases..."
+Masterpiece.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+user_bq = User.create(email: "bq@email.com", password: "password")
+user_mv = User.create(email: "mv@email.com", password: "password")
+user_gl = User.create(email: "gl@email.com", password: "password")
+user_fl = User.create(email: "fl@email.com", password: "password")
+
+puts "Creating masterpieces..."
+
+# Oeuvre 1
+masterpiece = Masterpiece.create(title: "Balancing", description: "Cette œuvre comprend du graphite, de l'encre et de la gouache, et est une combinaison d'exécutions intuitives et planifiées.", price: "1587", address: "Royaume-Uni", category: "paint", user_id: user_bq.id)
+# URL photo : https://media.artsper.com/artwork/652392_1_l.jpg
+
+puts "Created test #{masterpiece.title}"
+
+# Oeuvre 2
+Masterpiece.create(title: "Flamenco", description: "Cette œuvre créée en terre fut moulée pour être réalisée en 8 tirages et 4 épreuves d'artiste.", price: "4500", address: "France", category: "sculture", user_id: user_mv.id)
+# URL : https://media.artsper.com/artwork/2144158_1_m.jpg
+
+# Oeuvre 3
+Masterpiece.create(title: "Mhysa", description: "À travers mes séries photographiques intitulées Wanderlust, j'explore le thème ethnique en incarnant mentalement des femmes du monde entier.", price: "1500", address: "France", category: "photo", user_id: user_gl.id)
+# URL : https://media.artsper.com/artwork/2173875_1_m.jpg
+
+# Oeuvre 4
+Masterpiece.create(title: "Bubbles and bonbons", description: "Née en 1971 à San Francisco, la peintre Amy Nelder appelle son œuvre « Pop Trompe L'oeil ».", price: "6580", address: "Etats-Unis", category: "paint", user_id: user_fl.id)
+# URL : https://media.artsper.com/artwork/2139686_1_m.jpg
