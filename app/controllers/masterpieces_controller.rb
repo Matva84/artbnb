9f2ea0fdce9fd9123375050d9c40c5724d7c76e8
francois lorfevre <francoislorfevre@hotmail.com>
2024-02-16 17:28:40 +0100

    view show des masterpieces

--- app/controllers/masterpieces_controller.rb
+++ app/controllers/masterpieces_controller.rb
@@ -1,27 +1,29 @@
 class MasterpiecesController < ApplicationController
+ # before_action :set_masterpiece, only: [:show, :destroy]
 
-def index
-  @masterpieces = Masterpiece.all
-end
+  def index
+    @masterpieces = Masterpiece.all
+  end
 
-def show
-end
+  def show
+    @masterpiece = Masterpiece.find(params[:id])
+  end
 
-def new
-end
+  def new
+  end
 
-def create
-end
+  def create
+  end
 
-def destroy
-end
+  def destroy
+  end
 
-private
+  private
 
-def set_masterpiece
-end
+  def set_masterpiece
+    # @masterpiece = Masterpiece.find(params[:id])
+  end
 
-def masterpiece_params
-end
-
+  def masterpiece_params
+  end
 end
