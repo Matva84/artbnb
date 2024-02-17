f463dcb43a5c5e1a1ab98108e1ce18281e373fff
francois lorfevre <francoislorfevre@hotmail.com>
2024-02-17 17:04:24 +0100

    seed remise en état appli ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_194201) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_210358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_194201) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
+    t.string "longitude"
+    t.string "latitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
