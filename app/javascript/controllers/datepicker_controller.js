51915a9ccc6f18117c7e1e6afaf8e6532aae98a0
francois lorfevre <francoislorfevre@hotmail.com>
2024-02-26 20:27:44 +0100

    menu deroulant bolder

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,10 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr"
-
+import { Controller } from "@hotwired/stimulus";
+import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, {
+      minDate: "today"
+    });
   }
 }
